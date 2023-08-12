ff50d4cd2463b3722b70727ca5f1d48b9f4141be
zogmih <142104335+zogmih@users.noreply.github.com>
2023-08-12 18:38:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameNameInput = document.getElementById("game-name");
+const suggestionsDiv = document.getElementById("suggestions");
+
+gameNameInput.addEventListener("input", function() {
+  // JavaScript код остается без изменений
+});
+
+document.getElementById("search-form").addEventListener("submit", function(event) {
+  // JavaScript код остается без изменений
+});
+
+function displaySuggestions(suggestions) {
+  // JavaScript код остается без изменений
+}
+
+function displayMatchingGames(matchingGames) {
+  // JavaScript код остается без изменений
+}
